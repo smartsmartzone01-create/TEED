bdb41a86fe072e5aee6705c914f0cd363c386679
smartsmartzone <smartsmartzone01@gmail.com>
2026-08-17 16:39:31 +0300

    Fix trade-in migration base fields

--- backend/apps/commerce/migrations/0011_trade_in_transaction_details.py
+++ backend/apps/commerce/migrations/0011_trade_in_transaction_details.py
@@ -1,5 +1,6 @@
-from django.db import migrations, models
+import common.database.uuid
 import django.db.models.deletion
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -36,7 +37,7 @@ class Migration(migrations.Migration):
                 (
                     "id",
                     models.UUIDField(
-                        default=None,
+                        default=common.database.uuid.generate_uuid,
                         editable=False,
                         primary_key=True,
                         serialize=False,
@@ -44,6 +45,8 @@ class Migration(migrations.Migration):
                 ),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
                 ("updated_at", models.DateTimeField(auto_now=True)),
+                ("is_deleted", models.BooleanField(db_index=True, default=False)),
+                ("deleted_at", models.DateTimeField(blank=True, null=True)),
                 ("incoming_item_name", models.CharField(max_length=160)),
                 ("incoming_item_details", models.JSONField(blank=True, default=dict)),
                 ("incoming_value", models.DecimalField(decimal_places=2, max_digits=14)),
