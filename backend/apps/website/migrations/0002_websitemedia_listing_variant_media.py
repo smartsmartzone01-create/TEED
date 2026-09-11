69f41eb755d4676a0c578e12fce274debaec4da6
smartsmartzone <smartsmartzone01@gmail.com>
2026-09-12 00:28:51 +0300

    style(website): sort media migration imports

--- backend/apps/website/migrations/0002_websitemedia_listing_variant_media.py
+++ backend/apps/website/migrations/0002_websitemedia_listing_variant_media.py
@@ -1,6 +1,6 @@
 import common.database.uuid
-from django.db import migrations, models
 import django.db.models.deletion
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
